26a0385533ad19f56ba94830c0e5a328c7dcb1fe
Tangent5969 <82731644+Tangent5969@users.noreply.github.com>
2023-11-21 22:28:40 +0000

    made better use of snake class

--- core/src/com/snake/game/Game.java
+++ core/src/com/snake/game/Game.java
@@ -20,6 +20,7 @@ public class Game extends ApplicationAdapter {
 	int appleX, appleY;
 	boolean add = false;
 	public static ArrayList<Snake> snake = new ArrayList<>();
+	Head head;
 
 	public void apple() {
 		while (true) {
@@ -41,12 +42,51 @@ public class Game extends ApplicationAdapter {
 	}
 
 	public void setBoard() {
-		snake.add(new Snake(2, 20, 1));
+		head = new Head(2, 20, 1);
 		snake.add(new Snake(1, 20, 1));
 		snake.add(new Snake(0, 20, 1));
 		apple();
 	}
-	
+
+	public void logic() {
+		head.getMove();
+		if (timer == 0) {
+			for (int i = snake.size() - 1; i >= 0; i--) {
+				Snake current = snake.get(i);
+
+				if (add) {
+					snake.add(new Snake(current.x, current.y, current.direction));
+					score ++;
+					if (score > highScore) highScore++;
+					apple();
+					add = false;
+				}
+
+				if (current.x == head.x && current.y == head.y) {
+					lose();
+					return;
+				}
+
+				current.move();
+				if (i > 0) current.direction = snake.get(i - 1).direction;
+				else current.direction = head.direction;
+			}
+			head.move();
+			if (head.x == 40 || head.x == -1 || head.y == 40 || head.y == -1) {
+				lose();
+				return;
+			}
+			else if (head.x == appleX && head.y == appleY) {
+				add = true;
+			}
+
+			// controls tick speed
+			timer = 10;
+		}
+		timer --;
+	}
+
+
 	@Override
 	public void create () {
 		sr = new ShapeRenderer();
@@ -58,77 +98,17 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void render () {
 		ScreenUtils.clear(Color.BLACK);
-
-		// input WASD
-		if (Gdx.input.isKeyPressed(51)) snake.get(0).direction = 0;
-		else if (Gdx.input.isKeyPressed(32)) snake.get(0).direction = 1;
-		else if (Gdx.input.isKeyPressed(47)) snake.get(0).direction = 2;
-		else if (Gdx.input.isKeyPressed(29)) snake.get(0).direction = 3;
-
-		// input arrows
-		if (Gdx.input.isKeyPressed(19)) snake.get(0).direction = 0;
-		else if (Gdx.input.isKeyPressed(22)) snake.get(0).direction = 1;
-		else if (Gdx.input.isKeyPressed(20)) snake.get(0).direction = 2;
-		else if (Gdx.input.isKeyPressed(21)) snake.get(0).direction = 3;
-
-		if (timer == 0) {
-			for (int i = snake.size() - 1; i >= 0; i--) {
-				Snake current = snake.get(i);
-
-				// generates new segment
-				if (add) {
-					snake.add(new Snake(current.x, current.y, current.direction));
-					score ++;
-					if (score > highScore) highScore++;
-					apple();
-					add = false;
-				}
-
-				if (i != 0 && current.x == snake.get(0).x && current.y == snake.get(0).y) {
-					lose();
-					return;
-				}
-
-				// moves snake
-				switch (current.direction) {
-					case 0:
-						current.y ++;
-						break;
-					case 1:
-						current.x ++;
-						break;
-					case 2:
-						current.y --;
-						break;
-					case 3:
-						current.x --;
-						break;
-				}
-
-				if (i > 0) {
-					current.direction = snake.get(i - 1).direction;
-				}
-				else if (current.x == 40 || current.x == -1 || current.y == 40 || current.y == -1) {
-					lose();
-					return;
-				}
-				else if (current.x == appleX && current.y == appleY) {
-					add = true;
-				}
-			}
-			timer = 10;
-		}
+		logic();
 
 		// draws snake + apple
 		sr.begin(ShapeRenderer.ShapeType.Filled);
 		sr.setColor(Color.RED);
 		sr.rect(appleX * 20 + 2, appleY * 20 + 2, 18, 18);
 		sr.setColor(Color.GREEN);
+		sr.rect(head.x * 20 + 2, head.y * 20 + 2, 18, 18);
 		for (Snake i : snake) {
 			sr.rect(i.x * 20 + 2, i.y * 20 + 2, 18, 18);
 		}
-
-		timer --;
 
 		// draws top bar
 		sr.setColor(Color.DARK_GRAY);
--- core/src/com/snake/game/Snake.java
+++ core/src/com/snake/game/Snake.java
@@ -1,5 +1,7 @@
 package com.snake.game;
 
+import com.badlogic.gdx.Gdx;
+
 public class Snake {
 
     int x, y, direction;
@@ -8,6 +10,43 @@ public class Snake {
         this.x = x;
         this.y = y;
         this.direction = direction;
+    }
+
+    public void move() {
+        switch (direction) {
+            case 0:
+                y ++;
+                break;
+            case 1:
+                x ++;
+                break;
+            case 2:
+                y --;
+                break;
+            case 3:
+                x --;
+                break;
+        }
+    }
+}
+
+class Head extends Snake {
+    Head(int x, int y, int direction) {
+        super(x, y, direction);
+    }
+
+    public void getMove() {
+        // input WASD
+        if (Gdx.input.isKeyPressed(51)) direction = 0;
+        else if (Gdx.input.isKeyPressed(32)) direction = 1;
+        else if (Gdx.input.isKeyPressed(47)) direction = 2;
+        else if (Gdx.input.isKeyPressed(29)) direction = 3;
+
+        // input arrows
+        if (Gdx.input.isKeyPressed(19)) direction = 0;
+        else if (Gdx.input.isKeyPressed(22)) direction = 1;
+        else if (Gdx.input.isKeyPressed(20)) direction = 2;
+        else if (Gdx.input.isKeyPressed(21)) direction = 3;
     }
 }
 
